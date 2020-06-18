67111d8765ccb301815b22ff536a7b2e6dc13b5c
huydeptrai9x <buikh@DESKTOP-21H4ESO>
2020-06-18 23:02:23 +0700

    lam toi cuoi trang 44

--- BigSchool/Controllers/AttendancesController.cs
+++ BigSchool/Controllers/AttendancesController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using BigSchool.DTOs
 
 namespace BigSchool.Controllers
 {
@@ -20,12 +21,14 @@ namespace BigSchool.Controllers
         }
 
         [HttpPost]
-        public IHttpActionResult Attend([FromBody] int courseId)
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
         {
             var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
+                return BadRequest("The Attendance already exists!");
             var attendance = new Attendance
             {
-                CourseId = courseId,
+                CourseId = attendanceDto.CourseId,
                 AttendeeId = userId
             };
             _dbContext.Attendances.Add(attendance);
